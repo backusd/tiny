aa9ede7af9185a63ff3e29dd0f198e46d6f658b6
Dakota Backus <backusdakota@gmail.com>
2023-05-14 10:36:17 -0700

    Add project files.

---
+++ tiny/src/tiny/Test.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "Test.h"
+
+namespace tiny
+{
+	void Hello::print()
+	{
+		std::cout << "Hello" << std::endl;
+	}
+
+}
---
+++ tiny/src/tiny/Test.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "pch.h"
+#include "Core.h"
+
+#include <iostream>
+
+namespace tiny
+{
+	class TINY_API Hello
+	{
+	public:
+		static void print();
+	};
+
+}
